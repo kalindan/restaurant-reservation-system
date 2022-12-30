1c17ac1ed9f62942eeb3675fc2071270eb10ccc4
Kalindan <dikey@email.cz>
2022-12-30 14:13:10 +0100

    add table creation

--- reservation-system/sqlite_handler.go
+++ reservation-system/sqlite_handler.go
@@ -7,6 +7,44 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+const (
+	createCustomersTable string = `CREATE TABLE IF NOT EXISTS customers (
+		id TEXT PRIMARY KEY,
+		name	TEXT UNIQUE,
+		password	TEXT);`
+	createReservationsTable string = `CREATE TABLE IF NOT EXISTS reservations (
+		"id"	TEXT UNIQUE,
+		"customerId"	TEXT,
+		"tableId"	INTEGER,
+		"day"	INTEGER,
+		"hour"	INTEGER,
+		"duration"	INTEGER,
+		"persons"	INTEGER,
+		FOREIGN KEY("tableId") REFERENCES "Table"("id"),
+		FOREIGN KEY("customerId") REFERENCES "Customer"("id"),
+		PRIMARY KEY("id"));`
+	createTablesTable string = `CREATE TABLE IF NOT EXISTS tables (
+		"id"	INTEGER UNIQUE,
+		"seats"	INTEGER,
+		PRIMARY KEY("id"));`
+	createTimeslotsTable string = `CREATE TABLE IF NOT EXISTS timeslots (
+		"tableId"	INTEGER,
+		"1"	INTEGER,
+		"2"	INTEGER,
+		"3"	INTEGER,
+		"4"	INTEGER,
+		"5"	INTEGER,
+		"6"	INTEGER,
+		"7"	INTEGER,
+		"8"	INTEGER,
+		"9"	INTEGER,
+		"10"	INTEGER,
+		"11"	INTEGER,
+		"12"	INTEGER,
+		FOREIGN KEY("tableId") REFERENCES "Table"("id")
+	);`
+)
+
 type sqliteHandler struct {
 	db *sql.DB
 }
@@ -14,6 +52,18 @@ type sqliteHandler struct {
 func NewSqliteHandler() (*sqliteHandler, error) {
 	db, err := sql.Open("sqlite3", "rs.db")
 	if err != nil {
+		return nil, err
+	}
+	if _, err := db.Exec(createCustomersTable); err != nil {
+		return nil, err
+	}
+	if _, err := db.Exec(createReservationsTable); err != nil {
+		return nil, err
+	}
+	if _, err := db.Exec(createTablesTable); err != nil {
+		return nil, err
+	}
+	if _, err := db.Exec(createTimeslotsTable); err != nil {
 		return nil, err
 	}
 	sqhl := &sqliteHandler{
